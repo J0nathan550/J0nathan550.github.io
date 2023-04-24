b15ec0e79e49e2271d68fcad92a598d37b0e065f
J0nathan550 <ihudokormov@gmail.com>
2023-04-24 20:27:07 +0300

    Updated User Counter to understand if it is work.

--- Dodge Brick/game.js
+++ Dodge Brick/game.js
@@ -1,10 +1,13 @@
 var counterContainer = document.querySelector(".website-counter");
 var visitCount = localStorage.getItem("page_view");
 
-visitCount = 1;
-localStorage.setItem("page_view", 1);
-
-visitCount = Number(visitCount) + 1;
-localStorage.setItem("page_view", visitCount);
+// Check if page_view entry is present
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+  } else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+  }
 
 counterContainer.innerHTML = "Gamers played game: " + visitCount;
